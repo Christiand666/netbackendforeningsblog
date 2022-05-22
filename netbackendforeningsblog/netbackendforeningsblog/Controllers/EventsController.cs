bd275a8821d18703560a3ed64aabd5b2b12e4214
Yusuf1404 <34272892+Yusuf1404@users.noreply.github.com>
2022-05-22 21:30:56 +0200

    Delete event

--- netbackendforeningsblog/netbackendforeningsblog/Controllers/EventsController.cs
+++ netbackendforeningsblog/netbackendforeningsblog/Controllers/EventsController.cs
@@ -139,12 +139,12 @@ namespace netbackendforeningsblog.Controllers
 
         [HttpDelete("{id}")]
         [Authorize(Role.Admin)]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public Event Delete(int id)
         {
-            var @event = await _context.Events.FindAsync(id);
-            _context.Events.Remove(@event);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Get));
+            var eventid = _context.Events.Find(id);
+            _context.Events.Remove(eventid);
+            _context.SaveChanges();
+            return eventid;
         }
 
         private bool EventExists(int id)
